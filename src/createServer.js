84a5f0c09e9307c20d0b36c1e0efb79e95bedab4
Oksana Kuziv <oksanakuziv25@gmail.com>
2025-10-28 11:32:28 +0100

    fix

--- src/createServer.js
+++ src/createServer.js
@@ -8,7 +8,7 @@ function createServer() {
 
     if (req.method !== 'GET') {
       res.statusCode = 404;
-      res.end('Not Found');
+      res.end(JSON.stringify({ message: 'Not Found' }));
 
       return;
     }
@@ -45,7 +45,7 @@ function createServer() {
 
     if (errors.length > 0) {
       res.statusCode = 400;
-      res.statusMessage = 'Bad Request';
+      res.statusMessage = 'Bad request';
       res.end(JSON.stringify({ errors }));
 
       return;
